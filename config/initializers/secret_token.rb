7a98ac6293529a8b4f03d5ca2247af87484cbac0
Steve Woodcock <swoodcock@scholastic.co.uk>
2013-10-03 11:17:21 +0100

    Empty Rails 3.1.12 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Citest::Application.config.secret_token = 'b1498e2413fcd645b38e9c65d0ead908359ca999db8a8acafa00452619a6bdc81aaf03b5c316d7c91523c604721cb38c868f5460c6cfc465b0cbb0a1c907faf7'
